651c69cc697279297ed2654e6ef87d4c648d06a5
Beamma <jbremma@gmail.com>
2023-11-23 22:16:16 +1300

    Counter that update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      <Counter />
+      {/* <Message /> */}
       {/* <Greet name="Joel" heroname="Batman">
         <p>This is children props</p></Greet>
       <Greet name="Liam" heroname="Superman"/>
